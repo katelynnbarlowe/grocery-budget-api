c090794b50198a27fa8c5d5af9e4db4c4dc8e61f
Katelynn <katelynn.barlowe@gmail.com>
2020-10-27 19:49:30 -0400

    opening up api for testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080','http://localhost:8081'
+    #origins 'http://localhost:8080','http://localhost:8081'
+    origins '*'
 
     resource '*',
       headers: :any,
